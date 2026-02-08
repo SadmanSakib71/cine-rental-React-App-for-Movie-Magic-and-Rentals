9db8abfc5b06f979d4e416dd7cc6a0b4598a348a
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-02-08 19:18:22 +0600

    import-images

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./component/Header";
+import Sidebar from "./component/Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
