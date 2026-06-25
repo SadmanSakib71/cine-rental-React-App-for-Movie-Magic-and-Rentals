43ff942c78f6f3fc8ac87956b6dcd48f5aba8492
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-06-25 18:25:50 +0600

    text

--- src/component/cine/MovieModal.jsx
+++ src/component/cine/MovieModal.jsx
@@ -31,7 +31,7 @@ const MovieModal = ({ onCartAdd, movie, onClose }) => {
                   onClick={() => onCartAdd(movie)}
                 >
                   <img src="./assets/tag.svg" alt="" />
-                  <span>${movie.price}| Add to the Cart</span>
+                  <span>${movie.price}| Add to Cart</span>
                 </a>
                 <a
                   className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] dark:text-gray-200 font-semibold text-sm"
