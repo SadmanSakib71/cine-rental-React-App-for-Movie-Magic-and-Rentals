cc7a6ac3ab724dec6a7263ae31970049acf9c1a8
Sadman Sakib <155054940+SadmanSakib71@users.noreply.github.com>
2026-07-01 19:08:23 +0600

    text

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
       <footer className="py-6 md:py-8 mt-8">
         <div className="container mx-auto">
           <p className="text-center text-sm text-black/30 dark:text-[#EEEEEE] ">
-            Copyright ©2026 | All rights reserved by Built For Bangladesh
+            Copyright ©2026 | All rights reserved by Built For Bangladesh (B4B)
           </p>
         </div>
       </footer>
